c7fbbc2f733fe3be0ac1bebaa48d45940aad9646
stjoo0925 <stjoo0925@gmail.com>
2024-12-06 20:31:36 +0900

    feat: music upload

---
+++ src/pages/music/MusicUpload.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import PageLayout from "../../components/ui/PageLayout";
+import FileUploadCard from "../../components/music/FileUploadCard";
+import { useMusicApi } from "../../hooks/useMusicApi";
+import { useGenreSelect } from "../../hooks/useGenreSelect";
+import { Genre } from "../../types/music";
+
+const MusicUpload: React.FC = () => {
+    const { uploadMusic } = useMusicApi();
+    const { genres, selectedGenre, setSelectedGenre } = useGenreSelect();
+
+    const handleGenreChange = (genre: string) => {
+        setSelectedGenre(genre as Genre);
+    };
+
+    const handleUpload = async (file: File) => {
+        try {
+            await uploadMusic(file, selectedGenre);
+        } catch (error) {
+            console.error('File upload error:', error);
+        }
+    };
+
+    return (
+        <PageLayout title="음악 버킷 업로드">
+            <div className="flex justify-center gap-6">
+                <div className="w-1/3">
+                    <FileUploadCard
+                        genres={genres as Genre[]}
+                        selectedGenre={selectedGenre}
+                        onGenreChange={handleGenreChange}
+                        onFileSelect={handleUpload}
+                    />
+                </div>
+            </div>
+        </PageLayout>
+    );
+};
+
+export default MusicUpload;
